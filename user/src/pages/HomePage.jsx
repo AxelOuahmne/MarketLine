6ca3555f52e7a74a8a1c5604dc6bfaeac3886eb3
Axel Ouahmane <axel.ouahmane@gmail.com>
2024-04-28 16:27:27 +0200

    create folder user and admin react & larvel

---
+++ user/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, corrupti!</p>
+      </Fragment>
+    );
+  }
+}
+
+export default HomePage;
